68846e9f31089369694b4dd02123b14c0731c0ef
Nataliia Kotelnitska <83007830+Natali-Kotelnitska@users.noreply.github.com>
2021-12-02 19:22:45 +0300

    update

---
+++ Repeta/js/02_1-array.js
@@ -0,0 +1,48 @@
+const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+console.table(friends);
+// const lastIndex = friends.length - 1;
+
+// for (let i = 0; i < friends.length; i += 1) {
+//   friends[i] += `-${i}`;
+//   console.log(friends[i])
+// }
+
+// for (let friend of friends) {
+//   console.log(friend);
+// }
+
+// console.table(friends);
+
+// * PAYMENT
+//  * Посчитать общую сумму покупок в корзине
+//  */
+
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// 2 сделать переменную total до цикла
+let total = 0;
+
+// 1 перебрать массив
+// for (let i = 0; i < cart.length; i += 1) {
+//   // console.log(cart[i]);
+//   // 3 каждый элемент приплюслвать к total
+//   total += cart[i];
+//   // total = total + cart[i];
+// }
+
+for (const value of cart) {
+  total += value;
+}
+
+console.log('Total: ', total);
+
+// Добавить такс
+
+// for (let i = 0; i < cart.length; i += 1) {
+//   cart[i] = Math.round(cart[i] * 1.1);
+// }
+
+// for (let value of cart) {
+//   value = Math.round(value * 1.1);
+// }
+
+// console.log(cart);
