c509835d67c7cc7ca25cb13c5171b7572f5e5fe8
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-12-08 00:24:55 +0200

    Update 02_1-array.js

--- Repeta/js/02_1-array.js
+++ Repeta/js/02_1-array.js
@@ -1,5 +1,5 @@
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-console.table(friends);
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
 // const lastIndex = friends.length - 1;
 
 // for (let i = 0; i < friends.length; i += 1) {
@@ -17,23 +17,23 @@ console.table(friends);
 //  * Посчитать общую сумму покупок в корзине
 //  */
 
-const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
 // 2 сделать переменную total до цикла
-let total = 0;
+// let total = 0;
 
 // 1 перебрать массив
 // for (let i = 0; i < cart.length; i += 1) {
-//   // console.log(cart[i]);
+// console.log(cart[i]);
 //   // 3 каждый элемент приплюслвать к total
 //   total += cart[i];
 //   // total = total + cart[i];
 // }
 
-for (const value of cart) {
-  total += value;
-}
+// for (const value of cart) {
+//   total += value;
+// }
 
-console.log('Total: ', total);
+// console.log('Total: ', total);
 
 // Добавить такс
 
@@ -46,3 +46,28 @@ console.log('Total: ', total);
 // }
 
 // console.log(cart);
+
+function findLongestWord(string) {
+  const convertedString = string.split(' ');
+  console.log(convertedString);
+
+  let wordLength = convertedString[0].length;
+  // console.log('length: ', convertedString[0].length);
+  // console.log(convertedString[0]);
+
+  let longestWord;
+  for (let i = 0; i < convertedString.length; i += 1) {
+    // console.table(convertedString[i]);
+    // console.log(convertedString[i].length);
+    if (convertedString[i].length > wordLength) {
+      console.log(i);
+      console.log(convertedString[i]);
+      // longestWord = convertedString[i];
+      // return longestWord;
+    }
+  }
+  // return longestWord;
+}
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// console.log(findLongestWord('Google do a roll'));
+// console.log(findLongestWord('May the force be with you'));
