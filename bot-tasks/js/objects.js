928268ffb23ff366199335bb68530dc3fd2ff398
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-12-18 22:09:26 +0200

    Update objects.js

--- bot-tasks/js/objects.js
+++ bot-tasks/js/objects.js
@@ -55,12 +55,12 @@
 // }
 
 // {
-  // Module 3. Task 16/41. (Object.keys()) i for ...of.
-  // Напиши функцию countTotalSalary(salaries) которая
-  // принимает объект зарплат, где имя свойства это имя сотрудника,
-  //  а значение свойства это зарплата.
-  // Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её.
-  // Используй переменную totalSalary для хранения общей суммы зарплаты.
+// Module 3. Task 16/41. (Object.keys()) i for ...of.
+// Напиши функцию countTotalSalary(salaries) которая
+// принимает объект зарплат, где имя свойства это имя сотрудника,
+//  а значение свойства это зарплата.
+// Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её.
+// Используй переменную totalSalary для хранения общей суммы зарплаты.
 //   function countTotalSalary(salaries) {
 //     let totalSalary = 0;
 //     // Change code below this line
@@ -79,55 +79,53 @@
 // }
 
 {
-// Перебери массив объектов colors используя цикл for...of.
-// Добавь в массив hexColors значения свойств hex,
-//     а в массив rgbColors значения свойств rgb из всех
-//     объектов массива colors.
-  
+  // Перебери массив объектов colors используя цикл for...of.
+  // Добавь в массив hexColors значения свойств hex,
+  //     а в массив rgbColors значения свойств rgb из всех
+  //     объектов массива colors.
+
   const colors = [
-  { hex: '#f44336', rgb: '244,67,54' },
-  { hex: '#2196f3', rgb: '33,150,243' },
-  { hex: '#4caf50', rgb: '76,175,80' },
-  { hex: '#ffeb3b', rgb: '255,235,59' },
-];
+    { hex: '#f44336', rgb: '244,67,54' },
+    { hex: '#2196f3', rgb: '33,150,243' },
+    { hex: '#4caf50', rgb: '76,175,80' },
+    { hex: '#ffeb3b', rgb: '255,235,59' },
+  ];
 
-const hexColors = [];
-const rgbColors = [];
-// Change code below this line
-for (const color of colors){
-  hexColors.push(color.hex);
-  rgbColors.push(color.rgb);
-}
-
+  const hexColors = [];
+  const rgbColors = [];
+  // Change code below this line
+  for (const color of colors) {
+    hexColors.push(color.hex);
+    rgbColors.push(color.rgb);
+  }
 }
 
 {
   // Module 3. Task 18/41.
   // Задача. Поиск объекта по значению свойства
   // Напиши функцию getProductPrice(productName) которая принимает один параметр
-  // productName - название продукта.Функция ищет объект продукта с таким именем(свойство name) 
+  // productName - название продукта.Функция ищет объект продукта с таким именем(свойство name)
   // в массиве products и возвращает его цену(свойство price).
   // Если продукт с таким названием не найден, функция должна возвращать null.
-  
-  const products = [
-  { name: 'Radar', price: 1300, quantity: 4 },
-  { name: 'Scanner', price: 2700, quantity: 3 },
-  { name: 'Droid', price: 400, quantity: 7 },
-  { name: 'Grip', price: 1200, quantity: 9 },
-];
 
-function getProductPrice(productName) {
-  // Change code below this line
-  for (const product of products) {
-    if(product.name === productName) {
-      return product.price;
-    } 
-    
+  const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+
+  function getProductPrice(productName) {
+    // Change code below this line
+    for (const product of products) {
+      if (product.name === productName) {
+        return product.price;
+      }
+    }
+    return null;
+
+    // Change code above this line
   }
-  return null;
-  
-  // Change code above this line
-}
 
   // console.log(getProductPrice("Radar")); //1300
   // console.log(getProductPrice("Grip")); //1200
@@ -137,42 +135,98 @@ function getProductPrice(productName) {
 }
 
 {
+  // Module 3. Task 19/41.
+  // Напиши функцию getAllPropValues(propName) которая принимает
+  // один параметр propName - имя(ключ) свойства.
+  // Функция должна вернуть массив всех значений свойства
+  // с таким именем из каждого объекта в массиве products.
+  // Если в объектах нет свойства с таким именем,
+  //   функция должна вернуть пустой массив.
   const products = [
+    { name: 'Radar', price: 1300, quantity: 4 },
+    { name: 'Scanner', price: 2700, quantity: 3 },
+    { name: 'Droid', price: 400, quantity: 7 },
+    { name: 'Grip', price: 1200, quantity: 9 },
+  ];
+
+  // Варіант 1. Object.getOwnproperty(obj)
+  function getAllPropValues(propName) {
+    // Change code below this line
+    //варіант 1.
+
+    const newArray = [];
+    for (const product of products) {
+      if (product[propName]) newArray.push(product[propName]);
+    }
+    return newArray;
+  }
+
+  // варіант 2.
+  // let valueArray = [];
+  // for (const product of products) {
+  //   // console.log(product);
+  //   const ownProperty = Object.getOwnPropertyNames(product);
+  //   // console.log(ownProperty);
+  //   if (ownProperty.includes(propName)) {
+  //     valueArray.push(product[propName]);
+  //   }
+  // }
+  // return valueArray;
+
+  //* варіант 3. (Object.keys(obj))
+  // function getAllPropValues(propName) {
+  // Change code below this line
+  // const arrayOfValues = [];
+  // for (const product of products) {
+  // const keys = Object.keys(product);
+
+  //   if (keys.includes(propName)) {
+  //     arrayOfValues.push(product[propName]);
+  //     // console.log(arrayOfValues);
+  //   }
+  // }
+  // return arrayOfValues;
+  // }
+}
+
+// Change code above this line
+
+// console.log(getAllPropValues('name')); //["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues('quantity')); //[4, 3, 7, 9]
+// console.log(getAllPropValues('price')); //[1300, 2700, 400, 1200]
+// console.log(getAllPropValues('category'));
+
+const products = [
   { name: 'Radar', price: 1300, quantity: 4 },
   { name: 'Scanner', price: 2700, quantity: 3 },
   { name: 'Droid', price: 400, quantity: 7 },
   { name: 'Grip', price: 1200, quantity: 9 },
 ];
 
-function getAllPropValues(propName) {
-  // Change code below this line
-  const arrayOfValues = [];
-
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+  let totalPrice = 0;
   for (const product of products) {
-    if (propName === Object.keys(product)) {
-      arrayOfValues.push(product.name);
-      console.log(arrayOfValues)
+    // console.log(product.price);
+    if (product.name === productName) {
+      totalPrice = product.price * product.quantity;
     }
   }
-    // const keys = Object.keys(product);
-    // console.log(keys);
-    // if (keys.includes(propName)) {
-      // console.log(keys[0])
-      // arrayOfValues.push(product[name])
-    }
 
-    // for (const key of keys) {
-    //   console.log(key)
-    //   if (key === propName) {
-    //     console.log (product[key])
-    //     // arrayOfValues.push(product[key]);
-    //   }
-    // }
-    // arrayOfValues.push(product[key]);
-  }
-  
-  
+  return totalPrice;
+  // Пиши код выше этой строки
+
+  // console.log(calculateTotalPrice('Blaster')); //0
+  // console.log(calculateTotalPrice('Radar')); //5200
+  // console.log(calculateTotalPrice('Droid')); //2800
+  // console.log(calculateTotalPrice('Grip')); //10800
+  // console.log(calculateTotalPrice('Scanner')); //8100
+}
+
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+  result = message.toLowerCase().includes('spam' || 'sale');
   // Change code above this line
-
-  console.log(getAllPropValues("name")); //["Radar", "Scanner", "Droid", "Grip"]
-  // console.log(getAllPropValues("quantity")); //[4, 3, 7, 9]
+  return result;
+}
